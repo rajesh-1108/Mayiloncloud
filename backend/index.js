d69c474b429b3e3f7d40831188891e9c44c2b718
rajesh-1108 <64431251+rajesh-1108@users.noreply.github.com>
2026-01-08 10:28:26 +0530

    Update allowed origins for CORS

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  "https://yourdomain.com",
+  "https://mayiloncloud.netlify.app",
 ];
 
 app.use(
